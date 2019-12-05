ba4df03c2128ee0e75a9acb05b73701bb9129b8f
kimtimmy93 <kimtimmy93@gmail.com>
2019-12-04 19:43:20 -0800

    add files

---
+++ gatsby-starter-prologue/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Prologue', // <title>
+  manifestName: 'Prologue',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/images/portfolio-img.png',
+  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Timothy Kim',
+  heading: 'Software Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/kimtimmy93',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/timhkim/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:timhkim0623@gmail.com',
+    },
+  ],
+};
